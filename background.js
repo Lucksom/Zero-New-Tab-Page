70816e96c1e4cfae8973b67dd659e0d9eb1be8a9
Lucksom <ar9536385611@gmail.com>
2026-08-06 20:36:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,32 @@
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (!request || !request.ask) return true;
+
+  const tabId = sender.tab ? sender.tab.id : null;
+
+  switch (request.ask) {
+    case "backward":
+      chrome.tabs.goBack(tabId).catch(() => {});
+      break;
+    case "forward":
+      chrome.tabs.goForward(tabId).catch(() => {});
+      break;
+    case "reload":
+      chrome.tabs.reload(tabId);
+      break;
+    case "history":
+      chrome.tabs.create({ url: "chrome://history" });
+      break;
+    case "downloads":
+      chrome.tabs.create({ url: "chrome://downloads" });
+      break;
+    case "new_window":
+      chrome.windows.create({ 
+        url: "chrome://newtab", type: "popup", focused: true, state: "normal"
+      }, (win) => {
+        if (chrome.runtime.lastError) chrome.tabs.create({ url: "chrome://newtab" });
+      });
+      break;
+  }
+  return true; 
+});
